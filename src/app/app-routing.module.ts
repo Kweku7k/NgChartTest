538c191da8ae4eabaafbb2b1298011bfa648ad13
Kweku7k <nkba@live.com>
2020-06-30 07:51:51 +0000

    Initial COmmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyBarChartComponent } from '../app/my-bar-chart/my-bar-chart.component';
+import { MyDoughnutChartComponent } from '../app/my-doughnut-chart/my-doughnut-chart.component';
+import { MyRadarChartComponent } from '../app/my-radar-chart/my-radar-chart.component';
+import { MyPieChartComponent } from '../app/my-pie-chart/my-pie-chart.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bar-chart', component: MyBarChartComponent},
+  {path: 'doughnut-chart', component: MyDoughnutChartComponent},
+  {path: 'radar-chart', component: MyRadarChartComponent},
+  {path: 'pie-chart', component: MyPieChartComponent},
+  {path: '**', component: MyBarChartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
